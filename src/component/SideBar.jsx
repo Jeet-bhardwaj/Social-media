9c91cdf55bede67cf5c5dae8f01b2365192e4cfd
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-04-18 20:59:51 +0530

    SideBar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 const SideBar = ({sidebarItem , setSideBarItem}) => {
 
     return <>
-    <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: 280}}>
+    <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: 280, height : 918}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
       <span className="fs-4">BELLE REVE</span>
